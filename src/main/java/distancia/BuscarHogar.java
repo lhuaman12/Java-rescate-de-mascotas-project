b4fe2a13877b5e13f1e2486c786224428044ef05
xyz-frba <afraile@frba.utn.edu.ar>
2021-06-09 06:08:40 -0300

    Agrego cálculo de distancia para hogares de tránsito

--- src/main/java/distancia/BuscarHogar.java
+++ src/main/java/distancia/BuscarHogar.java
@@ -1,6 +1,7 @@
-package services.refugiodds;
+package distancia;
 
 import domain.TipoMascota;
+import services.refugiodds.ServicioRefugioDdS;
 import services.refugiodds.entidades.Hogar;
 import services.refugiodds.entidades.ListadoDeHogares;
 import services.refugiodds.entidades.Ubicacion;
@@ -8,7 +9,7 @@ import services.refugiodds.entidades.Ubicacion;
 import java.io.IOException;
 
 
-import static distancia.Distancia.distance;
+import static distancia.Distancia.calcularDistancia;
 
 public class BuscarHogar {
 
@@ -16,18 +17,14 @@ public class BuscarHogar {
 
         ServicioRefugioDdS servicioRefugioDdS = ServicioRefugioDdS.getInstancia();
 
-        // ListadoDeHogares listadoDeHogares = null;
-
         ListadoDeHogares listadoDeHogares = servicioRefugioDdS.listadoDeHogares(1);
-
 
         Hogar hogarMasCercano = new Hogar();
 
         double lat1 = ubicacion.getLat();
         double lon1 = ubicacion.getLong();
 
-        hogarMasCercano.nombre = "";
-        double min = 9999999999999999999999.999999;
+        double minDistancia = -1;
 
         for (Hogar hogar : listadoDeHogares.hogares) {
 
@@ -42,12 +39,10 @@ public class BuscarHogar {
             double lat2 = hogar.ubicacion.getLat();
             double lon2 = hogar.ubicacion.getLong();
 
-            double distancia = distance(lat1, lon1, lat2, lon2);
+            double distancia = calcularDistancia(lat1, lon1, lat2, lon2);
 
-            //System.out.println(distancia + "\t" + hogar.nombre + "\n");
-
-            if (distancia < min) {
-                min = distancia;
+            if (distancia < minDistancia || minDistancia < 0) {
+                minDistancia = distancia;
                 hogarMasCercano = hogar;
             }
         }
--- src/main/java/distancia/Distancia.java
+++ src/main/java/distancia/Distancia.java
@@ -1,4 +1,25 @@
 package distancia;
 
 public class Distancia {
+
+    public static double calcularDistancia(double lat1, double lon1, double lat2, double lon2) {
+
+        final int R = 6371; // Radio de la Tierra
+
+        double latDistancia = Math.toRadians(lat2 - lat1);
+        double lonDistancia = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistancia / 2) * Math.sin(latDistancia / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistancia / 2) * Math.sin(lonDistancia / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double distancia = R * c;
+
+        return distancia;
+    }
+
 }
+
+
