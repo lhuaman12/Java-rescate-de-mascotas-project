98fb12674a0918c0f031c68be0956ff4eddd54df
luhua1212 <lhuamanvargas@frba.utn.edu.ar>
2021-09-19 20:31:47 -0300

    solidarizo cosas del seminario

--- src/main/java/domain/entities/EntidadPersistente.java
+++ src/main/java/domain/entities/EntidadPersistente.java
@@ -1,14 +1,18 @@
-package db;
+package domain.entities;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class EntidadPersistente {
+public class EntidadPersistente {
     @Id
     @GeneratedValue
     private int id;
+
+    public void setId(int id){
+        this.id = id;
+    }
 
     public int getId() {
         return id;
