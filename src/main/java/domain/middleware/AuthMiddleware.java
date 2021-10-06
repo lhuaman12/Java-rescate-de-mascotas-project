744f9d69b40bc7fb7ae802c9f9c1dfeec91f7a97
xyz-frba <afraile@frba.utn.edu.ar>
2021-10-06 10:49:19 -0300

    New package: 'Middleware'

--- src/main/java/domain/middleware/AuthMiddleware.java
+++ src/main/java/domain/middleware/AuthMiddleware.java
@@ -1,2 +1,14 @@
-package domain.middleware;public class AuthMiddleware {
+package domain.middleware;
+
+import spark.Request;
+import spark.Response;
+
+public class AuthMiddleware {
+
+    public Response verificarSesion(Request request, Response response){
+        if(!request.session().isNew()){
+            response.redirect("/usuarios");
+        }
+        return response;
+    }
 }
