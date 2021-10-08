3113b11ff6e075f40dc46589d1d18bf39d394fd0
xyz-frba <afraile@frba.utn.edu.ar>
2021-10-08 05:43:01 -0300

    Add UsuarioController

--- src/main/java/domain/controllers/UsuarioController.java
+++ src/main/java/domain/controllers/UsuarioController.java
@@ -1,4 +1,14 @@
 package domain.controllers;
 
+import domain.entities.usuarios.Usuario;
+import domain.repositories.Repositorio;
+import domain.repositories.factories.FactoryRepositorio;
+
 public class UsuarioController {
+
+    private Repositorio<Usuario> repositorio;
+
+    public UsuarioController() {
+        this.repositorio = FactoryRepositorio.get(Usuario.class);
+    }
 }
