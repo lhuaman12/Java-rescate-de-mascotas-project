bfb921293f6c6ea80645ab9926e5689c6f77b6e7
xyz-frba <afraile@frba.utn.edu.ar>
2021-10-07 23:55:00 -0300

    DebugScreen

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,10 +3,12 @@ package server;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
+import static config.Config.sparkPort;
+
 public class Server {
     public static void main(String[] args) {
-        Spark.port(9000);
-        //Router.init();
+        Spark.port(sparkPort);
+        Router.init();
         DebugScreen.enableDebugScreen();
     }
 }
