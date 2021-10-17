c369c3ccfa74cf67b71c998cf0f8ccaff3e034fa
xyz-frba <afraile@frba.utn.edu.ar>
2021-10-16 21:49:10 -0300

    Update w/Rescate

--- src/test/java/database/EntityManagerTest.java
+++ src/test/java/database/EntityManagerTest.java
@@ -1,11 +1,19 @@
 package database;
 
 import db.EntityManagerHelper;
+import domain.entities.domicilio.Domicilio;
+import domain.entities.organizaciones.Organizacion;
+import domain.entities.rescate.RescateConQR;
+import domain.entities.rescate.RescateSinQR;
 import domain.entities.usuarios.Login;
 import domain.entities.usuarios.TipoDocumento;
 import domain.entities.usuarios.Usuario;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 
 public class EntityManagerTest {
 
@@ -36,6 +44,88 @@ public class EntityManagerTest {
 
         EntityManagerHelper.beginTransaction();
         EntityManagerHelper.getEntityManager().persist(usuario);
+        EntityManagerHelper.commit();
+    }
+
+    @Test
+    public void persistirRescateConQRTest() {
+
+        // Organización
+        Organizacion organizacion = new Organizacion();
+        organizacion.setNombre("Organizacion 1");
+        organizacion.setDescripcion("Descripción Organización 1");
+
+        // Rescatista
+        Usuario rescatista = new Usuario();
+        rescatista.setNombre("Juan");
+        rescatista.setApellido("Rescatista");
+        rescatista.setTipoDocumento(TipoDocumento.DNI);
+        rescatista.setNroDocumento("123456789");
+
+        // Dueño
+        Usuario duenio = new Usuario();
+        duenio.setNombre("Juan");
+        duenio.setApellido("Dueño");
+        duenio.setTipoDocumento(TipoDocumento.DNI);
+        duenio.setNroDocumento("987654321");
+
+        // Domicilio rescate
+        Domicilio domicilio = new Domicilio();
+        domicilio.setCalle("Calle");
+        domicilio.setAltura(1234);
+
+        // Rescate con QR
+        RescateConQR rescateConQR = new RescateConQR();
+        rescateConQR.setRescatista(rescatista);
+        rescateConQR.setDomicilio(domicilio);
+        rescateConQR.setDescripcion("Descripción del estado en que se encontró a la mascota con QR.");
+        rescateConQR.setLocalDateTime(LocalDateTime.now());
+        rescateConQR.setDuenio(duenio);
+        rescateConQR.setOrganizacion(organizacion);
+
+        EntityManagerHelper.beginTransaction();
+        EntityManagerHelper.getEntityManager().persist(organizacion);
+        EntityManagerHelper.getEntityManager().persist(rescatista);
+        EntityManagerHelper.getEntityManager().persist(duenio);
+        EntityManagerHelper.getEntityManager().persist(domicilio);
+        EntityManagerHelper.getEntityManager().persist(rescateConQR);
+        EntityManagerHelper.commit();
+
+    }
+
+    @Test
+    public void persistirRescateSinQRTest() {
+
+        // Organización
+        Organizacion organizacion = new Organizacion();
+        organizacion.setNombre("Organizacion 2");
+        organizacion.setDescripcion("Descripción Organización 2");
+
+        // Rescatista
+        Usuario rescatista = new Usuario();
+        rescatista.setNombre("Pedro");
+        rescatista.setApellido("Rescatista");
+        rescatista.setTipoDocumento(TipoDocumento.DNI);
+        rescatista.setNroDocumento("123456789");
+
+        // Domicilio rescate
+        Domicilio domicilio = new Domicilio();
+        domicilio.setCalle("Calle");
+        domicilio.setAltura(5678);
+
+        // Rescate sin QR
+        RescateSinQR rescateSinQR = new RescateSinQR();
+        rescateSinQR.setRescatista(rescatista);
+        rescateSinQR.setDomicilio(domicilio);
+        rescateSinQR.setDescripcion("Descripción del estado en que se encontró a la mascota sin QR.");
+        rescateSinQR.setLocalDateTime(LocalDateTime.now());
+        rescateSinQR.setOrganizacion(organizacion);
+
+        EntityManagerHelper.beginTransaction();
+        EntityManagerHelper.getEntityManager().persist(organizacion);
+        EntityManagerHelper.getEntityManager().persist(rescatista);
+        EntityManagerHelper.getEntityManager().persist(domicilio);
+        EntityManagerHelper.getEntityManager().persist(rescateSinQR);
         EntityManagerHelper.commit();
     }
 
