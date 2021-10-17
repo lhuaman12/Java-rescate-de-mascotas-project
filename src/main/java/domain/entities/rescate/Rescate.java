8887bf52e2b2f019156ddd6fee250a2fd24c3b9c
xyz-frba <afraile@frba.utn.edu.ar>
2021-10-16 21:48:43 -0300

    Rescate + RescateConQR + RescateSinQR

--- src/main/java/domain/entities/rescate/Rescate.java
+++ src/main/java/domain/entities/rescate/Rescate.java
@@ -1,16 +1,113 @@
 package domain.entities.rescate;
 
-import domain.entities.mascotas.MascotaPerdida;
+import domain.entities.domicilio.Domicilio;
+import domain.entities.mascotas.FotoMascota;
+import domain.entities.organizaciones.Organizacion;
 import domain.entities.usuarios.Contacto;
-import domain.entities.usuarios.MensajeMascotaPerdida;
 import domain.entities.usuarios.Usuario;
-import domain.entities.utils.notificador.notificables.Mensaje;
 
-public class Rescate {
-    private Usuario duenio;
-    private MascotaPerdida mascotaPerdida;
-    private EstadoMascotaPerdida estadoMascota = EstadoMascotaPerdida.CON_RESCATISTA;
-    private Usuario rescatista=null;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "rescate")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "qr")
+public abstract class Rescate {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "organizacion_id")
+    private Organizacion organizacion;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "rescatista_id")
+    private Usuario rescatista;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;            // Lugar en que se encontró la mascota.
+
+    @Column
+    private String descripcion;             // Descripción del estado en que se encontró la mascota.
+
+    @Transient
+    private List<FotoMascota> fotosMascota;       // Al menos una foto de la mascota perdida.
+
+    @Column(columnDefinition = "DATETIME", name="datetime")
+    private LocalDateTime localDateTime;
+
+
+    // Constructor
+    public Rescate() {
+        this.fotosMascota = new ArrayList<>();
+    }
+
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getRescatista() {
+        return rescatista;
+    }
+
+    public void setRescatista(Usuario rescatista) {
+        this.rescatista = rescatista;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<FotoMascota> getFotosMascota() {
+        return fotosMascota;
+    }
+
+    public void setFotosMascota(List<FotoMascota> fotosMascota) {
+        this.fotosMascota = fotosMascota;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public Organizacion getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(Organizacion organizacion) {
+        this.organizacion = organizacion;
+    }
+
+    // Methods
 
     public void avisarMascotaEncontrada(Contacto contacto){ // del rescatista al duenio
         //MensajeMascotaPerdida mensaje = new MensajeMascotaPerdida(duenio,);
--- src/main/java/domain/entities/rescate/RescateSinQR.java
+++ src/main/java/domain/entities/rescate/RescateSinQR.java
@@ -1,2 +1,41 @@
-package domain.entities.rescate;public class RescateSinQR {
+package domain.entities.rescate;
+
+import domain.entities.mascotas.MascotaPerdida;
+import domain.entities.usuarios.Usuario;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("sin_qr")
+public class RescateSinQR extends Rescate {
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "duenio_id")
+    private Usuario duenio;
+
+    @Transient
+    private MascotaPerdida mascotaPerdida;
+
+
+    // Constructor
+    public RescateSinQR() {}
+
+
+    // Getters and Setters
+
+    public Usuario getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(Usuario duenio) {
+        this.duenio = duenio;
+    }
+
+    public MascotaPerdida getMascotaPerdida() {
+        return mascotaPerdida;
+    }
+
+    public void setMascotaPerdida(MascotaPerdida mascotaPerdida) {
+        this.mascotaPerdida = mascotaPerdida;
+    }
 }
